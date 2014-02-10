8e7224a8380b4eb5890a75325e452eb129874c15
pianist1119 <pianist1119@gmail.com>
2014-02-09 20:23:51 -0500

    the setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.core import setup
+
+setup(name='Spritelibs for Pygame',
+      version='1.0',
+      description='Spritelibs is an extension to pygame for creating sprite classes and background animation.',
+      author='Pianist1119',
+      author_email='pianist1119@gmail.com',
+      url='https://github.com/pianist1119/spritelib-for-pygame',
+      py_modules=['spritelibs'],
+     )
